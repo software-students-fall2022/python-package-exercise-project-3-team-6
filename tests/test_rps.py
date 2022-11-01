b74559109718faf0565c3d268b917ec2329f2f84
Brian Lin <blin00712@gmail.com>
2022-11-01 13:42:17 -0400

    11/1 - Brian deleted main test py, edited test rps

--- tests/test_rps.py
+++ tests/test_rps.py
@@ -1,18 +1,15 @@
 # import RockPaperScissorsFullGame.RPS as RPS
-
 import os
 import sys
-
-sys.path.append(os.path.abspath("./src/RockPaperScissorsFullGame"))
-
-import RPS as RPS
-
 import pytest
+sys.path.append(os.path.abspath("./src/RockPaperScissorsFullGame"))
+import RPS as RPS
 
 """
 For testing RPS.py
     def getComputerItem
     def getOutcome
+    def inputToItem
 """
 
 # test that RPS.getComputerItem will return an item that is either a rock, paper or scissor enum
@@ -23,7 +20,7 @@ def test_getComputerItem():
     scissors = RPS.Item(3)
     assert computerItem == rock or computerItem == paper or computerItem == scissors
 
-
+# test multiple outcomes
 @pytest.mark.parametrize("player_item, computer_item, outcome", [
     (RPS.Item.Rock, RPS.Item.Rock, RPS.Outcome.Tie),
     (RPS.Item.Rock, RPS.Item.Paper, RPS.Outcome.Lose),
@@ -36,46 +33,44 @@ def test_getComputerItem():
     (RPS.Item.Scissors, RPS.Item.Scissors, RPS.Outcome.Tie),
 ])
 
-# test multiple outcomes
-    
 def test_multipleOutcomes(player_item, computer_item, outcome):
     assert RPS.getOutcome(player_item, computer_item) is outcome
     
-    
-# @pytest.mark.parametrize("player_input, expected", [
-#     ('r', RPS.Item.Rock),
-#     ('R', RPS.Item.Rock),
-#     ('ROCK', RPS.Item.Rock),
-#     ('rOcK',RPS.Item.Rock),
-#     ('rock', RPS.Item.Rock),
-#     ('rat',  None),
-#     ('racket', None),
-#     ('p', RPS.Item.Paper),
-#     ('P', RPS.Item.Paper),
-#     ('PAPER', RPS.Item.Paper),
-#     ('pApER', RPS.Item.Paper),
-#     ('paper', RPS.Item.Paper),
-#     ('pistol', None),
-#     ('panther', None),
-#     ('s', RPS.Item.Scissors),
-#     ('S', RPS.Item.Scissors),
-#     ('scissor', RPS.Item.Scissors),
-#     ('scissors', RPS.Item.Scissors),
-#     ('SCISSORS', RPS.Item.Scissors),
-#     ('sciSSORS', RPS.Item.Scissors),
-#     ('scorch', None),
-#     ('scanner', None),
-#     ('scjdkods', None),
-#     ('pdsadsd', None),
-#     ('rodsjdsod', None),
-#     ('dunce', None),
-#     ('lagoon', None),
-#     ('monastery', None),
-#     ('937932', None),
-#     ('1.3932', None),
-#     ('#93jf', None),
-#     ('flavorful-93932-#', None)
-# ])
+# test def inputToItem with multiple parameters 
+@pytest.mark.parametrize("player_input, expected", [
+    ('r', RPS.Item.Rock),
+    ('R', RPS.Item.Rock),
+    ('ROCK', RPS.Item.Rock),
+    ('rOcK',RPS.Item.Rock),
+    ('rock', RPS.Item.Rock),
+    ('rat',  None),
+    ('racket', None),
+    ('p', RPS.Item.Paper),
+    ('P', RPS.Item.Paper),
+    ('PAPER', RPS.Item.Paper),
+    ('pApER', RPS.Item.Paper),
+    ('paper', RPS.Item.Paper),
+    ('pistol', None),
+    ('panther', None),
+    ('s', RPS.Item.Scissors),
+    ('S', RPS.Item.Scissors),
+    ('scissor', RPS.Item.Scissors),
+    ('scissors', RPS.Item.Scissors),
+    ('SCISSORS', RPS.Item.Scissors),
+    ('sciSSORS', RPS.Item.Scissors),
+    ('scorch', None),
+    ('scanner', None),
+    ('scjdkods', None),
+    ('pdsadsd', None),
+    ('rodsjdsod', None),
+    ('dunce', None),
+    ('lagoon', None),
+    ('monastery', None),
+    ('937932', None),
+    ('1.3932', None),
+    ('#93jf', None),
+    ('flavorful-93932-#', None)
+])
 
-# def test_inputToItem(player_input, expected):
-#     assert RPS.inputToItem(player_input) is expected
+def test_inputToItem(player_input, expected):
+    assert RPS.inputToItem(player_input) is expected
